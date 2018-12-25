760b6aaf228038387e241efd3516c3aeb5d171f4
Holden Lee <holdenl@princeton.edu>
2018-12-25 14:59:01 -0500

    update for purs 0.12.1

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,17 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
+import Effect (Effect)
 
 import Halogen as H
-import Halogen.HTML.Events.Indexed as HE
-import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Properties.Indexed as HP
-import Halogen.Util (awaitBody, runHalogenAff)
+import Halogen.HTML (HTML)
+import Halogen.HTML.Core as HC
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Elements as HH
+import Halogen.HTML.Properties as HP
+import Halogen.Aff.Util (awaitBody, runHalogenAff)
+--import Halogen.Aff.Driver (runUI)
+import Halogen.VDom.Driver (runUI)
 
 import Data.Array
 import Data.Tuple
@@ -26,39 +30,43 @@ initialState = { curText: "", items: [] }
 data Query a = AddItem a | UpdateText String a | DeleteItem Int a
 --define Query (Action)
 
-ui :: forall g. H.Component State Query g
-ui = H.component { render, eval }
+ui :: forall g. Applicative g => H.Component HTML Query State Void g
+ui = H.component { initialState : initialState', render, eval, receiver }
   where
+  initialState' :: State -> State
+  initialState' = const initialState
   render :: State -> H.ComponentHTML Query
   render state =
     HH.div_
       [ HH.ul_ $
         --map (\x -> HH.li_ [HH.text x]) state.items,
         --this is not modular - which is why we do stuff with parent/children!
-          map2 (\n x -> HH.li_ [HH.text x, HH.button [HE.onClick (HE.input_ (DeleteItem n))] [HH.text "Delete"]]) (0..((length (state.items))-1)) state.items,
+          map2 (\n x -> HH.li_ [HC.text x, HH.button [HE.onClick (HE.input_ (DeleteItem n))] [HC.text "Delete"]]) (0..((length (state.items))-1)) state.items,
         HH.input
           [ HP.value (state.curText)
           , HE.onValueChange (HE.input UpdateText)],
         HH.button
-          [ HE.onClick (HE.input_ AddItem)] [ HH.text "Add"]
+          [ HE.onClick (HE.input_ AddItem)] [ HC.text "Add"]
       ]
 
-  eval :: Query ~> H.ComponentDSL State Query g
+  eval :: Query ~> H.ComponentDSL State Query Void g
   eval q = case q of
     AddItem next -> do
-      H.modify (\state -> state { curText = "", items = state.curText:state.items })
+      _ <- H.modify (\state -> state { curText = "", items = state.curText:state.items })
       pure next
     UpdateText str next -> do
-      H.modify (\state -> state { curText = str})
+      _ <- H.modify (\state -> state { curText = str})
       pure next
     DeleteItem n next -> do
-      H.modify (\state -> state {items = fromMaybe state.items (deleteAt n state.items)})
+      _ <- H.modify (\state -> state {items = fromMaybe state.items (deleteAt n state.items)})
       pure next
+  receiver = const Nothing
 
-main :: Eff (H.HalogenEffects ()) Unit
+--(H.HalogenEffects ())
+main :: Effect Unit
 main = runHalogenAff do
   body <- awaitBody -- ?
-  H.runUI ui initialState body
+  runUI ui initialState body
 
 {-
                                   , formValueHTML state.email
